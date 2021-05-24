6a28331140fd83ceb8e8f7dee89f8a4ac42bb6dc
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-05-25 03:11:33 +0530

    sidebar

--- adminclient/src/App.js
+++ adminclient/src/App.js
@@ -1,13 +1,11 @@
 import SideBar from "./Components/SideBar/sideBar";
 import { Container} from "react-bootstrap"
-
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
       <div>
-        <SideBar className="w-100" style={{ maxWidth:"400px" }} />
+        <SideBar/>
+        {/* <Dashboard/> */}
       </div>
-    </Container>
   );
 }
 
