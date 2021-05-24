3185bdcfc25a6a0a4992efa56a4c62b964e24eee
Khush2809 <2019.khushboo.dhingra@ves.ac.in>
2021-05-24 20:49:20 +0530

    80% dashboard is done (responsive also)

--- adminclient/src/App.js
+++ adminclient/src/App.js
@@ -1,11 +1,16 @@
 import SideBar from "./Components/SideBar/sideBar";
-import { Container} from "react-bootstrap"
+import Dashboard from "./Components/Dashboard/Dashboard";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
       <div>
-        <SideBar className="w-100" style={{ maxWidth:"400px" }} />
+        <SideBar className="w-100" style={{ maxWidth: "400px" }} />
+        <Dashboard />
       </div>
     </Container>
   );
---
+++ adminclient/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import PowerSettingsNewRoundedIcon from "@material-ui/icons/PowerSettingsNewRounded";
+import Divider from "@material-ui/core/Divider";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(4),
+    textAlign: "center",
+    color: theme.palette.text.dark,
+    fontSize: "large",
+    marginInline: "5%",
+    borderRadius: "10",
+  },
+  button: {
+    marginLeft: "85%",
+    margin: "2%",
+  },
+  divider: {
+    margin: "2%",
+  },
+  //   paper1: {
+  //     padding: theme.spacing(4),
+  //     textAlign: "center",
+  //     color: theme.palette.text.dark,
+  //     fontSize: "large",
+  //     marginInline: "5",
+  //     borderRadius: "10",
+  //     borderRightColor: "secondary",
+  //   },
+}));
+
+const Dashboard = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Container maxWidth="lg">
+        {/* <Button
+          variant="outlined"
+          size="medium"
+          color="secondary"
+          startIcon={<PowerSettingsNewRoundedIcon />}
+          className={classes.button}
+        >
+          Log Out
+        </Button> */}
+        <Divider className={classes.divider} />
+      </Container>
+      <Grid className={classes.root} container spacing={2}>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper className={classes.paper}>80</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper className={classes.paper}>30</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper className={classes.paper}>9</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper className={classes.paper}>39</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Dashboard;
