ccc6dc930a90c7c2231df9194b08ee4ddac606a8
Khush2809 <2019.khushboo.dhingra@ves.ac.in>
2021-06-06 23:43:14 +0530

    done

--- adminclient/src/Components/TextEditor/AddCourseActivity.jsx
+++ adminclient/src/Components/TextEditor/AddCourseActivity.jsx
@@ -8,9 +8,79 @@ import "react-quill/dist/quill.snow.css";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
-import {useStyles} from "./../../Styles/AddCourseStyle"
 import { EditorState } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+const useStyles = makeStyles((theme) => ({
+  editor1: {
+    width: "70%",
+    marginLeft: "0%",
+  },
+  Header: {
+    fontSize: "2rem",
+    borderTopLeftRadius: "10px",
+    borderTopRightRadius: "10px",
+    backgroundColor: "#318ebd",
+    width: "90%",
+    color: "white",
+    border: "2px solid #318ebd",
+    padding: "2%",
+  },
+  borderbox1: {
+    border: "1px solid rgb(128, 128, 128, 0.4)",
+    width: "90%",
+    borderBottomLeftRadius: "10px",
+    borderBottomRightRadius: "10px",
+    marginLeft: "5%",
+    padding: "3%",
+    marginBottom: "2%",
+  },
+  paper1: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary,
+  },
+  paper: {
+    marginTop: "2%",
+    padding: theme.spacing(0.2),
+    color: theme.palette.text.secondary,
+  },
+  uploadimg: {
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    paddingBottom: "8%",
+  },
+  chooseFile: {
+    padding: "2%",
+  },
+  saveBtn: {
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    padding: "2%",
+    textAlign: "center",
+    marginTop: "3%",
+  },
+  addContent: {
+    display: "inline",
+    color: "#302e2e",
+    backgroundColor: "#f0f5f5",
+    letterSpacing: "0.1rem",
+    zIndex: "1",
+  },
+  dividerInset: {
+    margin: `3px 0 0 ${theme.spacing(15)}px`,
+    marginBottom: "-10px",
+  },
+  endDivider: {
+    padding: "0.05%",
+    marginTop: "6%",
+  },
+  submitBtn: {
+    textAlign: "center",
+    margin: "4%",
+    marginTop: "4%",
+    marginBottom: "4%",
+  },
+}));
 
 const formats = [
   "background",
