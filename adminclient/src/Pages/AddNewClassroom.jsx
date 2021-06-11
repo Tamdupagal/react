9ae662649ddf07dbae6dc292e6f29256d3d0eefe
Khush2809 <2019.khushboo.dhingra@ves.ac.in>
2021-06-11 23:17:33 +0530

    select multiple check

--- adminclient/src/Pages/AddNewClassroom.jsx
+++ adminclient/src/Pages/AddNewClassroom.jsx
@@ -1,11 +1,18 @@
-import React, {useState, useRef} from "react";
-import { Container } from "@material-ui/core";
+import React, { useState, useRef } from "react";
+import { Container, Select } from "@material-ui/core";
 import { Button, Card } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import MenuItem from "@material-ui/core/MenuItem";
-import {useStyles} from "./../Styles/AddnewClassroom"
-import Classroom from "./../CRUD/Classroom"
+import { useStyles } from "./../Styles/AddnewClassroom";
+import { useTheme } from "@material-ui/core/styles";
+import Chip from "@material-ui/core/Chip";
+import ListItemText from "@material-ui/core/ListItemText";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+
+import Classroom from "./../CRUD/Classroom";
 
 const AddNewClassroom = () => {
   const nameRef = useRef();
@@ -17,15 +24,62 @@ const AddNewClassroom = () => {
     Teacher: "",
     Students: "",
   };
-  const [classroom, setClassroom] = useState(newClassroomState)
-  
+  const names = [
+    "Oliver Hansen",
+    "Van Henry",
+    "April Tucker",
+    "Ralph Hubbard",
+    "Omar Alexander",
+    "Carlos Abbott",
+    "Miriam Wagner",
+    "Bradley Wilkerson",
+    "Virginia Andrews",
+    "Kelly Snyder",
+  ];
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 500,
+      },
+    },
+  };
+  const [classroom, setClassroom] = useState(newClassroomState);
+
   const [value, setValue] = useState("interview");
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
+  function getStyles(name, personName, theme) {
+    return {
+      fontWeight:
+        personName.indexOf(name) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    };
+  }
+
+  // const handleChange = (event) => {
+  //   setValue(event.target.value);
+  // };
   const classes = useStyles();
-  
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    setPersonName(event.target.value);
+  };
+
+  const handleChangeMultiple = (event) => {
+    const { options } = event.target;
+    const value = [];
+    for (let i = 0, l = options.length; i < l; i += 1) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
+    setPersonName(value);
+  };
   return (
     <div>
       <div>
@@ -55,7 +109,7 @@ const AddNewClassroom = () => {
                     size="small"
                     select
                     // ref={courseRef}
-                    onchange={handleChange}
+                    // onchange={handleChange}
                     className={classes.textField}
                   >
                     <MenuItem value={1}>interview</MenuItem>
@@ -72,7 +126,7 @@ const AddNewClassroom = () => {
                     size="small"
                     select
                     value={value}
-                    onchange={handleChange}
+                    // onchange={handleChange}
                     className={classes.textField}
                   >
                     <MenuItem value={"interview"}>interview</MenuItem>
@@ -82,15 +136,40 @@ const AddNewClassroom = () => {
                   </TextField>
                 </form>
                 <h5 className={classes.infoHeading}>Assign Students:</h5>
-                <form>
-                  {" "}
-                  <TextField
-                    id="outlined-basic"
-                    variant="outlined"
-                    size="small"
-                    className={classes.textField}
-                  />
-                </form>
+                <FormControl className={classes.formControl}>
+                  {/* <InputLabel id="demo-mutiple-chip-label">Chip</InputLabel> */}
+                  <Select
+                    // labelId="demo-mutiple-chip-label"
+                    // variant="outlined"
+                    id="demo-mutiple-chip"
+                    multiple
+                    value={personName}
+                    onChange={handleChange}
+                    input={<Input id="select-multiple-chip" />}
+                    renderValue={(selected) => (
+                      <div className={classes.chips}>
+                        {selected.map((value) => (
+                          <Chip
+                            key={value}
+                            label={value}
+                            className={classes.chip}
+                          />
+                        ))}
+                      </div>
+                    )}
+                    MenuProps={MenuProps}
+                  >
+                    {names.map((name) => (
+                      <MenuItem
+                        key={name}
+                        value={name}
+                        style={getStyles(name, personName, theme)}
+                      >
+                        {name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
                 <div className={classes.submitBtn}>
                   <Button variant="contained" color="secondary">
                     SUBMIT CLASSROOM
