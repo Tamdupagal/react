106970fd3bcc9a271e8e20f7f8f024e362ff6e9c
Khush2809 <2019khushboo.dhingra@ves.ac.in>
2021-07-18 00:46:05 +0000

    updated otp

--- adminclient/src/Pages/OtpSuccessfullySent.jsx
+++ adminclient/src/Pages/OtpSuccessfullySent.jsx
@@ -12,7 +12,7 @@ const history = useHistory()
         <div>
             <Box display="flex" justifyContent="center">
                 <Grid item xs={12} lg={7}>
-                    <h3 style={{position:"relative",textAlign:"center"}}>OTP has been sent to your Email I'd</h3>
+                    <h3 style={{position:"relative",textAlign:"center",fontFamily:"'Exo',sans-serif"}}>OTP has been sent to your Email I'd</h3>
                     <img src={otp} style={{ height: "80%", width: "100%" }}></img>
                     <Box display="flex" justifyContent="center">
                         <Button variant="contained" color="secondary" onClick={handleEnterOTP}>OK</Button>
