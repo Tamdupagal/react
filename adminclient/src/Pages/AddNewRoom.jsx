6996f82ec14202b39c8c909f477021dc009eacd6
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-07-24 01:12:50 +0530

    new room

--- adminclient/src/Pages/AddNewRoom.jsx
+++ adminclient/src/Pages/AddNewRoom.jsx
@@ -1,29 +1,35 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Container, Select } from "@material-ui/core";
 import { Button, Card, Box } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import MenuItem from "@material-ui/core/MenuItem";
 import { useStyles } from "./../Styles/AddNewRoom";
-import { getAllTeachers, getAllTrainers } from "../action/actions";
+import { addTrainee, getAllTeachers, getAllTrainers } from "../action/actions";
 import { AppContext } from "../AppContext";
 
 const AddNewRoom = () => {
   const {dispatch} = useContext(AppContext)
-  const [value, setValue] = React.useState("interview");
   const [allTeachers, setAllTeachers] = useState([])
+  const [allTrainers, setAllTrainers] = useState([])
+  const [trainerId,setTrainerId] = useState([])
+  const [traineeId,setTraineeId] = useState([])
+  const [role, setRole] = useState("")
   let trainers;
   let teachers;
   let teacherOptions = [];
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
+  const roomName = useRef()
   const classes = useStyles();
   useEffect(async() => {
     if(!trainers){
-      trainers = await getAllTrainers(dispatch)
-      console.log(trainers)
+      try{
+        trainers = await getAllTrainers(dispatch)
+        trainers.map(c=>console.log(c._id))
+        setAllTrainers(trainers)
+      }
+      catch (err) {
+        console.log(err)
+      }
     }
     if(!teachers){
       teachers = await getAllTeachers(dispatch)
@@ -33,17 +39,43 @@ const AddNewRoom = () => {
     }
   }, [])
 
+  const handleTraineeChange = (e) => {
+    e.preventDefault()
+    setTraineeId(e.target.value._id)
+  }
+
+  const handleTrainerChange = (e) => {
+    e.preventDefault()
+    setTrainerId(e.target.value._id)
+  }
+
+  const handleTraineeRole = (e) => {
+    e.preventDefault()
+    setRole(e.target.value)
+    console.log(e.target.value)
+  }
+
+  const handleSaveRoom = () =>{
+    var data = {
+      classroom_details: [roomName],
+      trainer_id: trainerId,
+      trainee_id: traineeId,
+      trainer_type: role
+    }
+    addTrainee(dispatch,data)
+    // history.push("/trainers")
+  }
   return (
     <div>
       <div>
-        <h1 className={classes.mainHeading}>Add a Classroom</h1>
+        <h1 className={classes.mainHeading}>Add a Room</h1>
       </div>
       <Container style={{ marginBottom: "20vh" }}>
         <div>
           <Box display="flex" justifyContent="center">
             <Grid item xs={12} lg={9}>
               <Card className={classes.card0}>
-                <h5 className={classes.infoHeading}>Classroom Name:</h5>
+                <h5 className={classes.infoHeading}>Room Name:</h5>
                 <form>
                   {" "}
                   <TextField
@@ -51,45 +83,31 @@ const AddNewRoom = () => {
                     variant="outlined"
                     size="small"
                     className={classes.textField}
+                    inputRef={roomName}
                   />
                 </form>
-                <h5 className={classes.infoHeading}>Trainer Type:</h5>
                 <h5 className={classes.infoHeading}>Assign Trainer:</h5>
                 <form>
-                  {" "}
-                  <TextField
-                    id="outlined-basic"
+                  {
+                     (
+                  <Select
                     variant="outlined"
                     size="small"
-                    select
-                    value={value}
-                    onchange={handleChange}
+                    onChange={handleTrainerChange}
                     className={classes.textField}
+                    defaultValue=""
                   >
-                    <MenuItem value={"interview"}>interview</MenuItem>
-                    <MenuItem value={"hii"}>hii</MenuItem>
-                    <MenuItem value={"hello"}>hello</MenuItem>
-                    <MenuItem value={"hola"}>hola</MenuItem>
-                  </TextField>
-                </form>
-                <h5 className={classes.infoHeading}>Assign Course:</h5>
-                <form>
-                  {" "}
-                  <TextField
-                    id="outlined-basic"
-                    variant="outlined"
-                    size="small"
-                    select
-                    value={value}
-                    onchange={handleChange}
-                    className={classes.textField}
-                  >
-                    <MenuItem value={"interview"}>interview</MenuItem>
-                    <MenuItem value={"hii"}>hii</MenuItem>
-                    <MenuItem value={"hello"}>hello</MenuItem>
-                    <MenuItem value={"hola"}>hola</MenuItem>
-                  </TextField>
-                </form>
+                    {
+                      allTrainers.map((trainer, index) => (
+                          <MenuItem key={index} value={trainer}>
+                            {trainer._id}
+                          </MenuItem>
+                        ))
+                    }
+                  </Select>
+                    )
+                  }
+                    </form>             
                 <h5 className={classes.infoHeading}>Trainee Type:</h5>
                 <form>
                 {" "}
@@ -99,7 +117,7 @@ const AddNewRoom = () => {
                     // value={ownRole}
                     // options={courseOptions}
                     // inputRef={courseRef}
-                    // onChange={handleOwnRoleChange}
+                    onChange={handleTraineeRole}
                     className={classes.textField}
                     defaultValue=""
                   >
@@ -119,7 +137,7 @@ const AddNewRoom = () => {
                   <Select
                     variant="outlined"
                     size="small"
-                    onChange={handleChange}
+                    onChange={handleTraineeChange}
                     className={classes.textField}
                     defaultValue=""
                   >
@@ -135,7 +153,9 @@ const AddNewRoom = () => {
                   }
                     </form>
                 <div className={classes.submitBtn}>
-                  <Button variant="contained" color="secondary">
+                  <Button variant="contained" color="secondary"
+                  onClick={handleSaveRoom}
+                  >
                     SUBMIT CLASSROOM
                   </Button>
                 </div>
