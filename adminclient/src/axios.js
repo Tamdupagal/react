6fdc8915b8e4e6226b9835db4e84e7db8ffe6827
Utkarsh_agrawal <jiren0359san@gmail.com>
2021-08-06 00:09:24 +0530

    Retrived lost work

--- adminclient/src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'http://35.223.212.87/admin/api',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      }
-});
